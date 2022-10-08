a116a582fb289dea419f1b21979958086f4deaea
hydrz <n.haoyuan@gmail.com>
2022-10-08 11:35:35 +0800

    wg

--- docker-compose/0-network/up.sh
+++ docker-compose/0-network/up.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -ex
+
+pushd $(dirname "$0")
+
+source ../.env
+
+# Create a bridge network to allow the containers to communicate
 docker network create -d bridge \
   --subnet=${SUBNET} \
   --ip-range=${IP_RANGE} \
   --gateway=${GATEWAY} \
   ${DOKCER_BRIDGE}
+
+popd
