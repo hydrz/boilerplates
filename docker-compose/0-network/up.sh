11429c435632446ae602252bbc773aebf051172c
hydrz <n.haoyuan@gmail.com>
2022-11-23 17:29:13 +0800

    k8s

--- docker-compose/0-network/up.sh
+++ docker-compose/0-network/up.sh
@@ -7,7 +7,7 @@ pushd $(dirname "$0")
 source ../.env
 
 # Create a bridge network to allow the containers to communicate
-docker network create -d bridge \
+podman network create -d bridge \
   --subnet=${SUBNET} \
   --ip-range=${IP_RANGE} \
   --gateway=${GATEWAY} \
